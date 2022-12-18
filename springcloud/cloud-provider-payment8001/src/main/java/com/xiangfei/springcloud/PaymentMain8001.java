e97a71de3c86aae181859f1a6ea46a45195fcfe2
xiangfei0409 <xiangfei0409@foxmail.com>
2022-12-18 20:44:20 +0800

    提交springcloud代码

---
+++ springcloud/cloud-provider-payment8001/src/main/java/com/xiangfei/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.xiangfei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: xiangfei
+ * @CreateTime: 2022/12/17 19:40
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
