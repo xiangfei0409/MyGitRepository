2692bfd1aaf9fbc0334e1bc6defe7e7d815e2604
xiangfei0409 <xiangfei0409@foxmail.com>
2022-12-18 13:09:45 +0800

    123

--- cloud-provider-payment8001/src/main/java/com/xiangfei/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/xiangfei/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.xiangfei.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: xiangfei
  * @CreateTime: 2022/12/17 19:40
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
